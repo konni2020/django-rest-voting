9d6573ed1547e199cbb0ece36ce3d06ec3e924a5
konni <test@test.com>
2019-01-05 22:18:14 +0800

    add models and test

--- voting_app/models.py
+++ voting_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.db.models import Count
 
-# Create your models here.
+class Poll(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+
+    @staticmethod
+    def valid_polls():
+        return Poll.objects.annotate(num_choices=Count('choice')) \
+                    .filter(num_choices__gt=1)
+
+
+
+class Choice(models.Model):
+    text = models.CharField(max_length=50)
+    total = models.PositiveIntegerField(default=0)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
--- voting_app/tests.py
+++ voting_app/tests.py
@@ -1,6 +1,94 @@
 from django.test import TestCase
+from voting_app.models import Poll, Choice
+
+TEST_POLL = {
+    'name': 'john',
+    'description': 'What fruit below do you like most?',
+}
 
 
-class ModelTest(TestCase):
+TEST_CHOICES = [
+    {
+        'text': 'apple',
+        'total': 3,
+    },
+    {
+        'text': 'pear',
+        'total': 5,
+    },
+    {
+        'text': 'watermelon',
+        'total': 10,
+    },
+]
 
-    def test_create_and_save_voting_model
+
+def create_a_poll():
+    poll = Poll()
+    for key, value in TEST_POLL.items():   
+        setattr(poll, key, value)
+
+    poll.save()
+    return poll
+
+
+class PollModelTest(TestCase):
+
+    def test_create_and_retrieve_poll(self):
+        poll = Poll()
+        for key, value in TEST_POLL.items():   
+            setattr(poll, key, value)
+        poll.save()
+
+        self.assertEqual(Poll.objects.count(), 1)
+
+        poll = Poll.objects.first()
+        for key, value in TEST_POLL.items():   
+            self.assertEqual(getattr(poll, key), value)
+
+    def test_valid_poll_should_has_more_than_one_choice(self):
+        poll = create_a_poll()
+        self.assertEqual(Poll.valid_polls().count(), 0)
+
+        for i in range(3):
+            choice = Choice.objects.create(text='test choice', poll=poll)
+
+        self.assertEqual(Poll.valid_polls().count(), 1)
+
+
+class ChoiceModelTest(TestCase):
+
+
+    def test_create_and_retrieve_choice(self):
+        poll = create_a_poll()
+        choice = Choice()
+        for key, value in TEST_CHOICES[0].items():
+            setattr(choice, key, value)
+        choice.poll = poll
+        choice.save()
+
+        self.assertEqual(Choice.objects.count(), 1)
+
+        choice = Choice.objects.first()
+        for key, value in TEST_CHOICES[0].items():   
+            self.assertEqual(getattr(choice, key), value)
+
+    def test_default_total_is_zero(self):
+        poll = create_a_poll()
+        Choice.objects.create(text='test choice', poll=poll)
+        choice = Choice.objects.get(text='test choice')
+
+        self.assertEqual(choice.total, 0)
+
+    def test_add_choices_to_poll(self):
+        poll = create_a_poll()
+        for choice_data in TEST_CHOICES:
+            choice = Choice()
+
+            for key, value in choice_data.items():
+                setattr(choice, key, value)
+
+            choice.poll = poll
+            choice.save()    
+
+        self.assertEqual(poll.choice_set.count(), 3)
