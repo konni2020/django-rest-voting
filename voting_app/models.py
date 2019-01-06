d22c0618e7b554e5dfb328d92161c41caf6f501f
konni <test@test.com>
2019-01-06 08:08:02 +0800

    alter models

--- voting_app/models.py
+++ voting_app/models.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.db import models
 from django.db.models import Count
 
@@ -5,6 +6,20 @@ class Poll(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
     modified = models.DateTimeField(auto_now=True)
+    format_modified = models.CharField(blank=True, null=True, max_length=100)
+
+    def __str__(self):
+        return '{id} - {desc}'.format(id=self.id, desc=self.description)
+
+    def save(self, *args, **kwargs):
+        self.save_format_modified()
+        super().save(*args, **kwargs)
+
+    def save_format_modified(self):
+        if self.modified:
+            self.format_modified = self.modified.strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            self.format_modified = timezone.now().strftime('%Y-%m-%d %H:%M:%S')
 
     @staticmethod
     def valid_polls():
@@ -12,8 +27,10 @@ class Poll(models.Model):
                     .filter(num_choices__gt=1)
 
 
-
 class Choice(models.Model):
     text = models.CharField(max_length=50)
     total = models.PositiveIntegerField(default=0)
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
