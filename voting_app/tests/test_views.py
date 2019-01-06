d9469eebb78a3361385584f60d6c409bf42c0191
konni <test@test.com>
2019-01-06 08:39:55 +0800

    add test for post detail

--- voting_app/tests/test_views.py
+++ voting_app/tests/test_views.py
@@ -1,5 +1,6 @@
 import json
 
+from django.forms.models import model_to_dict
 from django.shortcuts import reverse
 from django.test import TestCase
 
@@ -63,13 +64,10 @@ class PollAPITest(TestCase):
         return list(poll.choice_set.all().values('text', 'total'))
 
     def _get_poll_json_data(self, poll):
-        return {
-            'id': poll.id,
-            'name': poll.name,
-            'description': poll.description,
-            'format_modified': poll.format_modified,
-            'choices': self._get_choices_data(poll),
-        }
+        fields = ['id', 'name', 'description', 'format_modified']
+        data = model_to_dict(poll, fields=fields)
+        data['choices'] = self._get_choices_data(poll)
+        return data
 
     def _get_polls_json_data(self, polls):
         return [self._get_poll_json_data(poll) for poll in polls]
@@ -80,7 +78,16 @@ class PollAPITest(TestCase):
             'results': self._get_polls_json_data(Poll.objects.all())
         }
 
-        polls_view = reverse('polls:poll_list')
-        response = self.client.get(polls_view)
+        url = reverse('polls:poll_list')
+        response = self.client.get(url)
+
+        self.assertJSONEqual(response.content.decode(), data)
+
+    def test_poll_detail_view(self):
+        poll = create_a_poll_with_choices()
+        data = self._get_poll_json_data(poll)
+
+        url = reverse('polls:poll_detail', kwargs={'pk': poll.id})
+        response = self.client.get(url)
 
         self.assertJSONEqual(response.content.decode(), data)
