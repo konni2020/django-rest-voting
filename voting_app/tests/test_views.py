5debb4176c6aa342b18a52d2e39040c0ee7315ec
konni <test@test.com>
2019-02-06 04:46:33 +0800

    refactor BaseTest class

--- voting_app/tests/test_views.py
+++ voting_app/tests/test_views.py
@@ -42,76 +42,81 @@ TEST_CHOICES = [
 ]
 
 
-def create_user(data=None):
-    if not data:
-        data = TEST_USER
+class BaseTest(APITestCase):
 
-    user = UserSerializer(data=data)
-    if user.is_valid():
-        user.save()
+    @classmethod
+    def create_user(cls, data=None):
+        if not data:
+            data = TEST_USER
 
-    return user
+        user = UserSerializer(data=data)
+        if user.is_valid():
+            user.save()
+
+        return user
+
+    @classmethod
+    def create_a_poll(cls):
+        poll = Poll()
+        for key, value in TEST_POLL.items():
+            setattr(poll, key, value)
+        poll.save()
+
+        return poll
+
+    @classmethod
+    def create_a_choice(cls, poll, instance=None, data=None):
+        if instance:
+            text = instance.text
+            total = instance.total
+        elif data:
+            text = data['text']
+            total = data['total']
+        else:
+            text = TEST_CHOICES[0]['text']
+            total = 0
+
+        choice = Choice.objects.create(poll=poll, text=text, total=total)
+
+        return choice
+
+    @classmethod
+    def create_a_poll_with_choices(cls, ):
+        poll = cls.create_a_poll()
+
+        for choice_data in TEST_CHOICES:
+            cls.create_a_choice(poll, data=choice_data)
+
+        return poll
+
+    @classmethod
+    def get_choices_data(cls, poll):
+        # get choices json data from a poll
+        # -> [
+        # {'text': 'apple', 'total': 3},
+        # {'text': 'pear', 'total': 5},
+        # {'text': 'watermelon', 'total': 10}
+        # ]
+        return list(poll.choice_set.all().values('text', 'total'))
+
+    @classmethod
+    def get_poll_json_data(cls, poll):
+        fields = ['id', 'name', 'description', 'format_modified']
+        data = model_to_dict(poll, fields=fields)
+        data['choices'] = cls.get_choices_data(poll)
+        return data
+
+    @classmethod
+    def get_poll_list_json_data(cls, polls):
+        return [cls.get_poll_json_data(poll) for poll in polls]
 
 
-def create_a_poll():
-    poll = Poll()
-    for key, value in TEST_POLL.items():
-        setattr(poll, key, value)
-    poll.save()
-
-    return poll
-
-
-def create_a_choice(poll, instance=None, data=None):
-    if instance:
-        text = instance.text
-        total = instance.total
-    elif data:
-        text = data['text']
-        total = data['total']
-    else:
-        text = TEST_CHOICES[0]['text']
-        total = 0
-
-    choice = Choice.objects.create(poll=poll, text=text, total=total)
-
-    return choice
-
-
-def create_a_poll_with_choices():
-    poll = create_a_poll()
-
-    for choice_data in TEST_CHOICES:
-        create_a_choice(poll, data=choice_data)
-
-    return poll
-
-
-def get_choices_data(poll):
-    # get choices json data from a poll
-    # -> [
-    # {'text': 'apple', 'total': 3},
-    # {'text': 'pear', 'total': 5},
-    # {'text': 'watermelon', 'total': 10}
-    # ]
-    return list(poll.choice_set.all().values('text', 'total'))
-
-
-def get_poll_json_data(poll):
-    fields = ['id', 'name', 'description', 'format_modified']
-    data = model_to_dict(poll, fields=fields)
-    data['choices'] = get_choices_data(poll)
-    return data
-
-
-def get_polls_json_data(polls):
-    return [get_poll_json_data(poll) for poll in polls]
-
-
-class BaseTest(TestCase):
-
+class BaseViewTest(BaseTest):
+    """
+    it will auto create a user and login
+    """
     def setUp(self):
-        create_user()
+        self.create_user()
         self.auto_login()
 
     def auto_login(self):
@@ -121,11 +126,11 @@ class BaseTest(TestCase):
         )
 
 
-class PollAPITest(BaseTest):
+class PollAPITest(BaseViewTest):
 
     def test_post_list_view(self):
-        create_a_poll_with_choices()
-        data = get_polls_json_data(Poll.objects.all())
+        self.create_a_poll_with_choices()
+        data = self.get_poll_list_json_data(Poll.objects.all())
 
         url = reverse('polls:poll-list')
         response = self.client.get(url)
@@ -133,8 +138,8 @@ class PollAPITest(BaseTest):
         self.assertJSONEqual(response.content.decode(), data)
 
     def test_poll_detail_view(self):
-        poll = create_a_poll_with_choices()
-        data = get_poll_json_data(poll)
+        poll = self.create_a_poll_with_choices()
+        data = self.get_poll_json_data(poll)
 
         url = reverse('polls:poll-detail', kwargs={'pk': poll.id})
         response = self.client.get(url)
@@ -185,11 +190,11 @@ class PollAPITest(BaseTest):
         self.assertEqual(len(expect_keys), len(response_data.keys()))
 
 
-class ChoiceAPITest(BaseTest):
+class ChoiceAPITest(BaseViewTest):
 
     def test_choice_list_view(self):
-        poll = create_a_poll_with_choices()
-        data = get_choices_data(poll)
+        poll = self.create_a_poll_with_choices()
+        data = self.get_choices_data(poll)
 
         url = reverse('polls:choice-list', kwargs={'pk': poll.pk})
         response = self.client.get(url)
@@ -197,7 +202,7 @@ class ChoiceAPITest(BaseTest):
         self.assertJSONEqual(response.content.decode(), data)
 
     def test_create_choice_via_post_method_on_list_view(self):
-        poll = create_a_poll()
+        poll = self.create_a_poll()
 
         url = reverse('polls:choice-list', kwargs={'pk': poll.pk})
         response = self.client.post(url, data=TEST_CHOICES[0])
@@ -208,10 +213,10 @@ class ChoiceAPITest(BaseTest):
         self.assertJSONEqual(response.content.decode(), TEST_CHOICES[0])
 
 
-class TestToken(APITestCase):
+class TestToken(BaseTest):
 
     def setUp(self):
-        create_user()
+        self.create_user()
 
     def get_token(self):
         url = reverse('login')
