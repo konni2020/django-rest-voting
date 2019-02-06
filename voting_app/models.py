6c9e6ef1fb0eb145f75e7ea6b1cd318a9f0b5b31
konni <test@test.com>
2019-02-06 19:14:27 +0800

    add created to poll

--- voting_app/models.py
+++ voting_app/models.py
@@ -7,11 +7,12 @@ from django.contrib.auth.models import User
 class Poll(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     format_modified = models.CharField(blank=True, null=True, max_length=100)
 
     class Meta:
-        ordering = ('-modified',)
+        ordering = ('-created',)
 
     def __str__(self):
         return '{id} - {desc}'.format(id=self.id, desc=self.description)
