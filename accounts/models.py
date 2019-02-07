6c04b0de854d88d4f23e3d71ddc6ac3dc4fe06a2
konni <test@test.com>
2019-02-07 12:15:37 +0800

    custom User model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(
+        validators=[MinLengthValidator(5)],
+        max_length=20, unique=True)
+    password = models.CharField(
+        validators=[MinLengthValidator(6)],
+        max_length=20)
