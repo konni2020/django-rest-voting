a4f71763c5f73938924d8e32ef35dd7066e1223f
konni <test@test.com>
2019-02-07 12:16:40 +0800

    add tests for username and password length validator

--- accounts/tests/test_serializers.py
+++ accounts/tests/test_serializers.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 from accounts.serializers import UserSerializer
 
+
+User = get_user_model()
 
 TEST_USER = {
     'username': 'konni',
@@ -19,3 +21,23 @@ class UserSerializerTest(TestCase):
             serializer.save()
 
         self.assertEqual(User.objects.count(), 1)
+
+    def test_can_not_create_user_with_short_username(self):
+        data = TEST_USER.copy()
+        data['username'] = 'k'
+
+        serializer = UserSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+
+        self.assertEqual(User.objects.count(), 0)
+
+    def test_can_not_create_user_with_short_password(self):
+        data = TEST_USER.copy()
+        data['password'] = 'k'
+
+        serializer = UserSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+
+        self.assertEqual(User.objects.count(), 0)
--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -2,9 +2,12 @@ import json
 
 from django.test import TestCase
 from django.shortcuts import reverse
+from django.contrib.auth import get_user_model
 
 from accounts.serializers import UserSerializer
 
+
+User = get_user_model()
 
 TEST_USER = {
     'username': 'konni',
@@ -13,18 +16,43 @@ TEST_USER = {
 }
 
 
-class UserViewTest(TestCase):
+class RegisterViewTest(TestCase):
     def test_user_create(self):
         url = reverse('user_create')
         response = self.client.post(
             url, json.dumps(TEST_USER),
             content_type='application/json')
 
+        # should not return password in response
         self.assertNotIn('password', response.content.decode())
-        
+
         response_data = json.loads(response.content.decode())
         self.assertEqual(response_data['username'], TEST_USER['username'])
         self.assertEqual(response_data['email'], TEST_USER['email'])
+
+    def test_can_not_create_user_with_short_username(self):
+        data = TEST_USER.copy()
+        data['username'] = 'k'
+
+        url = reverse('user_create')
+        response = self.client.post(
+            url, json.dumps(data),
+            content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(User.objects.count(), 0)
+
+    def test_can_not_create_user_with_short_password(self):
+        data = TEST_USER.copy()
+        data['password'] = 'k'
+
+        url = reverse('user_create')
+        response = self.client.post(
+            url, json.dumps(data),
+            content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(User.objects.count(), 0)
 
 
 class LoginViewTest(TestCase):
