c160e0a4cc108477a0ff6a9b11752dfa5736b619
konni <test@test.com>
2019-02-09 19:02:56 +0800

    when creating user, should return token

--- accounts/apiviews.py
+++ accounts/apiviews.py
@@ -6,21 +6,35 @@ from django.contrib.auth import authenticate
 from accounts.serializers import UserSerializer
 
 
+def get_token(request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            return user.auth_token.key
+
+
 class RegisterView(generics.CreateAPIView):
     serializer_class = UserSerializer
     authentication_classes = ()
     permission_classes = ()
+
+    def post(self, request):
+        response = super().post(request)
+        token = get_token(request)
+        if token:
+            response.data['token'] = token
+        return response
 
 
 class LoginView(APIView):
     permission_classes = ()
 
     def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        user = authenticate(username=username, password=password)
-
-        if user:
-            return Response({'token': user.auth_token.key})
+        token = get_token(request)
+        if token:
+            return Response({'token': token})
         else:
-            return Response({'error': 'Wrong Credentials'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {'error': 'Wrong Credentials'},
+                status=status.HTTP_400_BAD_REQUEST)
